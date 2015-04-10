e126366185b0084fc3db4fef097ddbbd9fe86aa9
Vladimir Fishchenko <hws47a@gmail.com>
2015-04-10 14:49:47 +0300

    PCMI-45 convert function in lead entity

--- app/code/community/TNW/Salesforce/Model/Api/Entity/Lead.php
+++ app/code/community/TNW/Salesforce/Model/Api/Entity/Lead.php
@@ -7,4 +7,17 @@ class TNW_Salesforce_Model_Api_Entity_Lead extends TNW_Salesforce_Model_Api_Enti
         parent::_construct();
         $this->_init('tnw_salesforce_api_entity/lead');
     }
+
+    public function convert()
+    {
+        $prepareData = array(
+            'convertedStatus' => Mage::helper("tnw_salesforce")->getLeadConvertedStatus(),
+            'leadId' => $this->getId(),
+            'doNotCreateOpportunity' => 'true',
+            'overwriteLeadSource' => 'false',
+            'sendNotificationEmail' => 'false',
+        );
+
+        $response = Mage::getSingleton('tnw_salesforce/api_function')->convertLead($prepareData);
+    }
 }
--- app/code/community/TNW/Salesforce/Test/Model/Api/Entity/Lead.php
+++ app/code/community/TNW/Salesforce/Test/Model/Api/Entity/Lead.php
@@ -29,4 +29,31 @@ class TNW_Salesforce_Test_Model_Api_Entity_Lead extends TNW_Salesforce_Test_Case
         $entity->load($leadId);
         $this->assertEquals($expectation, $entity->getData());
     }
+
+    /**
+     * @loadFixture
+     * @dataProvider dataProvider
+     * @test
+     */
+    public function convert($leadId)
+    {
+        //mock api function model to check input params
+        $mockApiFunction = $this->mockModel('tnw_salesforce/api_function');
+        $mockApiFunction->expects($this->once())
+            ->method('convertLead')
+            ->with(array(
+                'convertedStatus' => Mage::helper("tnw_salesforce")->getLeadConvertedStatus(),
+                'leadId' => $leadId,
+                'doNotCreateOpportunity' => 'true',
+                'overwriteLeadSource' => 'false',
+                'sendNotificationEmail' => 'false',
+            ));
+        $this->replaceByMock('singleton', 'tnw_salesforce/api_function', $mockApiFunction);
+
+        /** @var TNW_Salesforce_Model_Api_Entity_Lead $lead */
+        $lead = Mage::getModel('tnw_salesforce_api_entity/lead')
+            ->setData($this->getSalesforceFixture('lead', array('ID' => $leadId)));
+
+        $lead->convert();
+    }
 }
