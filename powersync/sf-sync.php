2ce404d7c77b046ee4a37e9c74a13303008ebb57
Vladimir Fishchenko <hws47a@gmail.com>
2015-04-26 16:55:30 +0300

    PCMI-66 Refactor sf-sync.php file without update logic

--- powersync/sf-sync.php
+++ powersync/sf-sync.php
@@ -100,8 +100,6 @@ if (!$helper->isWorking()) {
             } else {
                 if (count($objects) == 1) {
                     // Process Realtime
-                    //$entity = array();
-                    //$queueStatus = true;
                     $object = $objects[0];
                     // Each object should have 'attributes' property and 'type' inside 'attributes'
                     if (property_exists($object, "attributes") && property_exists($object->attributes, "type")) {
@@ -110,44 +108,48 @@ if (!$helper->isWorking()) {
                             /* Safer to set the session at this level */
                             Mage::getSingleton('core/session')->setFromSalesForce(true);
                             // Call proper Magento upsert method
-                            if (
-                                $object->attributes->type == "Contact"
-                                || ($object->IsPersonAccount == 1 && $object->attributes->type == "Account")
-                            ) {
-                                if ($object->Email || (property_exists($object, 'IsPersonAccount') && $object->IsPersonAccount == 1 && $object->PersonEmail)) {
-                                    $helper->log("Synchronizing: " . $object->attributes->type);
-                                    //$entity[] = Mage::helper('tnw_salesforce/customer')->contactProcess($object);
-                                    Mage::helper('tnw_salesforce/magento_customers')->process($object);
-                                } else {
-                                    $helper->log("SKIPPING: Email is missing in Salesforce!");
-                                }
-                            } elseif ($object->attributes->type == $_prefix . "Website__c") {
-                                if (
-                                    property_exists($object, $_prefix . 'Website_ID__c')
-                                    && !empty($object->{$_prefix . 'Website_ID__c'})
-                                    && property_exists($object, $_prefix . 'Code__c')
-                                    && !empty($object->{$_prefix . 'Code__c'})
-                                ) {
-                                    Mage::helper('tnw_salesforce/magento_websites')->process($object);
-                                } else {
-                                    $helper->log("SKIPPING: Website ID and/or Code is missing in Salesforce!");
-                                }
-                            } elseif ($object->attributes->type == "Product2") {
-                                if ($object->ProductCode) {
-                                    Mage::helper('tnw_salesforce/magento_products')->process($object);
-                                } else {
-                                    $helper->log("SKIPPING: ProductCode is missing in Salesforce!");
-                                }
-                            } else {
-                                $helper->log("Synchronization SKIPPED for: " . $object->attributes->type);
+                            switch ($object->attributes->type) {
+                                case 'Account': //for account when personal account enabled
+                                    if ($object->IsPersonAccount != 1) {
+                                        break;
+                                    }
+                                    //we don\'t need break here because we use the same code as next
+                                case 'Contact': //or for contact
+                                    if ($object->Email || (property_exists($object, 'IsPersonAccount') && $object->IsPersonAccount == 1 && $object->PersonEmail)) {
+                                        $helper->log("Synchronizing: " . $object->attributes->type);
+                                        //$entity[] = Mage::helper('tnw_salesforce/customer')->contactProcess($object);
+                                        Mage::helper('tnw_salesforce/magento_customers')->process($object);
+                                    } else {
+                                        $helper->log("SKIPPING: Email is missing in Salesforce!");
+                                    }
+                                    break;
+                                case $_prefix . 'Website__c':
+                                    if (
+                                        property_exists($object, $_prefix . 'Website_ID__c')
+                                        && !empty($object->{$_prefix . 'Website_ID__c'})
+                                        && property_exists($object, $_prefix . 'Code__c')
+                                        && !empty($object->{$_prefix . 'Code__c'})
+                                    ) {
+                                        Mage::helper('tnw_salesforce/magento_websites')->process($object);
+                                    } else {
+                                        $helper->log("SKIPPING: Website ID and/or Code is missing in Salesforce!");
+                                    }
+                                    break;
+                                case 'Product2':
+                                    if ($object->ProductCode) {
+                                        Mage::helper('tnw_salesforce/magento_products')->process($object);
+                                    } else {
+                                        $helper->log("SKIPPING: ProductCode is missing in Salesforce!");
+                                    }
+                                    break;
+                                default:
+                                    $helper->log("Synchronization SKIPPED for: " . $object->attributes->type);
                             }
                             /* Reset session for further insertion */
                             Mage::getSingleton('core/session')->setFromSalesForce(false);
                         } catch (Exception $e) {
                             $helper->log("Error: " . $e->getMessage());
                             $helper->log("Failed to upsert a " . $object->attributes->type . " #" . $object->Id . ", please re-save or re-import it manually");
-                            //$queueStatus = false;
-                            unset($e);
                         }
                     }
                 } else {
@@ -167,19 +169,17 @@ if (!$helper->isWorking()) {
                     } catch (Exception $e) {
                         $errorString = "Could not process JSON, Error: " . $e->getMessage();
                         $helper->log($errorString);
-                        unset($e);
                     }
                 }
             }
         }
     }
 }
-$response->error = $errorString;
+
 /* Create JSON to send back to Salesforce */
+$response->error = $errorString;
 $return = json_encode($response);
+print($return);
 
 $helper->log("Return JSON to Salesforce: " . $return);
 $helper->log("========== Sync from Salesforce End ==========");
-
-echo $return; // display JSON for Salesforce
-unset($return);
