010783688fb55ede7085a933403733faefeead21
Evgeniy <eermolaev@yandex.ru>
2015-04-28 14:24:58 +0300

    PCMI-80 - Orders: only use prices and cost in base currency

--- app/code/community/TNW/Salesforce/data/tnw_salesforce_setup/data-upgrade-0.01.51.00.0005-0.01.51.00.0006.php
+++ app/code/community/TNW/Salesforce/data/tnw_salesforce_setup/data-upgrade-0.01.51.00.0005-0.01.51.00.0006.php
@@ -1,10 +1,13 @@
 <?php
+/**
+ * @var $installer Mage_Core_Model_Resource_Setup
+ */
 $installer = $this;
 
 /**
  * @comment Enable cache by default
  */
-$installer->getConnection()->insert(
+$installer->getConnection()->insertOnDuplicate(
     $installer->getTable('core/cache_option'), array(
         'code' => 'tnw_salesforce',
         'value' => 1,
