769f7dace688b8ef76ca88fc1bcec1cea97ee2ed
ikrasnykh <igorkrasnykh@gmail.com>
2015-05-04 07:57:29 -0500

    PCMI-109 Fixed a bug on the invoice page

--- app/code/community/TNW/Salesforce/Block/Sales/Order/View/Salesforce.php
+++ app/code/community/TNW/Salesforce/Block/Sales/Order/View/Salesforce.php
@@ -17,18 +17,30 @@ class TNW_Salesforce_Block_Sales_Order_View_Salesforce extends Mage_Adminhtml_Bl
     public function getSalesforceData()
     {
         $_labels = Mage::helper('tnw_salesforce/field')->getOrderLabels();
-
-        foreach ($_labels as $_field => $_label) {
-            $_value = $this->getOrder()->getData($_field);
-            if ($_value) {
-                $sfData[] = array(
-                    'label' => $_label,
-                    'value' => Mage::helper('tnw_salesforce/salesforce_abstract')->generateLinkToSalesforce($this->escapeHtml($_value, array('br')))
-                );
+        $sfData = array();
+        if ($this->doesHaveOrder()) {
+            foreach ($_labels as $_field => $_label) {
+                $_value = $this->getOrder()->getData($_field);
+                if ($_value) {
+                    $sfData[] = array(
+                        'label' => $_label,
+                        'value' => Mage::helper('tnw_salesforce/salesforce_abstract')->generateLinkToSalesforce($this->escapeHtml($_value, array('br')))
+                    );
+                }
             }
+            //ksort($accountData, SORT_NUMERIC);
         }
-        //ksort($accountData, SORT_NUMERIC);
 
         return $sfData;
+    }
+
+    public function doesHaveOrder() {
+        try {
+            $orderExists = ($this->getOrder() && is_object($this->getOrder())) ? true : false;
+        } catch (Exception $e) {
+            $orderExists = false;
+        }
+
+        return $orderExists;
     }
 }
