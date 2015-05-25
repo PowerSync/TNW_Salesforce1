8a83840a1df38234a30a01376ed1c0c5daa21800
Vladimir Fishchenko <hws47a@gmail.com>
2015-05-25 12:46:30 +0300

    PCMI-121 Move tnw_salesforce/blocks_observer/updateBlocks observer to adminhtml area and made a method refactoring to smaller impact this observer on other pages

--- app/code/community/TNW/Salesforce/Model/Blocks/Observer.php
+++ app/code/community/TNW/Salesforce/Model/Blocks/Observer.php
@@ -5,20 +5,26 @@
  */
 class TNW_Salesforce_Model_Blocks_Observer
 {
-    public function updateBlocks($observer) {
-        if (Mage::helper('tnw_salesforce')->isWorking()) {
+    /**
+     * Update adminhtml/sales_order_view_info html if module enabled
+     *
+     * @param Varien_Event_Observer $observer
+     */
+    public function updateBlocks(Varien_Event_Observer $observer)
+    {
+        $block = $observer->getEvent()->getBlock();
+        /** @var Mage_Core_Block_Abstract $block */
+        if ($block->getType() == 'adminhtml/sales_order_view_info'
+            && Mage::helper('tnw_salesforce')->isWorking()
+        ) {
             $normalOutput = $observer->getTransport()->getHtml();
-            $block = $observer->getBlock();
-            $layout = $block->getLayout();
-            if ($block->getType() == 'adminhtml/sales_order_view_info') {
-                $newBlock = $layout->createBlock(
-                    'TNW_Salesforce_Block_Sales_Order_View_Salesforce',
-                    'salesforce_info',
-                    array('template' => 'salesforce/sales/order/view/salesforce.phtml')
-                )->toHtml();
+            $newBlock = $block->getLayout()->createBlock(
+                'TNW_Salesforce_Block_Sales_Order_View_Salesforce',
+                'salesforce_info',
+                array('template' => 'salesforce/sales/order/view/salesforce.phtml')
+            )->toHtml();
 
-                $observer->getTransport()->setHtml($newBlock . $normalOutput);
-            }
+            $observer->getTransport()->setHtml($newBlock . $normalOutput);
         }
     }
 }
