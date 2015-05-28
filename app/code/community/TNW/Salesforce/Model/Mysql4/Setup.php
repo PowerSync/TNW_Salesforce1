96d0eba60109161c22e69f7335338b843fc9ecea
ikrasnykh <igorkrasnykh@gmail.com>
2015-05-27 20:42:27 -0500

    PCMI-125 Reverting back

--- app/code/community/TNW/Salesforce/Model/Mysql4/Setup.php
+++ app/code/community/TNW/Salesforce/Model/Mysql4/Setup.php
@@ -3,7 +3,7 @@
 /**
  * Class TNW_Salesforce_Model_Mysql4_Setup
  */
-class TNW_Salesforce_Model_Mysql4_Setup extends Mage_Core_Model_Resource_Setup
+class TNW_Salesforce_Model_Mysql4_Setup extends Mage_Sales_Model_Resource_Setup
 {
 
 }
