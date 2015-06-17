8f8b129af115d8b813146a5a42cfc996547698e0
Vladimir Fishchenko <hws47a@gmail.com>
2015-06-17 15:32:16 +0300

    PCMI-123 Fixed chosen

--- js/tnw-salesforce/chosen-enable.js
+++ js/tnw-salesforce/chosen-enable.js
@@ -1,5 +1,6 @@
 document.observe('dom:loaded', function () {
     $$('.chosen-select').each(function (element) {
-        new Chosen(element);
+        element.removeAttribute('disabled');
+        new Chosen(element, {width: '280px'});
     });
 });
