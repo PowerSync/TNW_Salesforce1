a8f6d4af799374f33615a96e3a13c570fcfed2db
ikrasnykh <igorkrasnykh@gmail.com>
2015-07-01 22:22:18 -0500

    Opportunity Id block on order bage is now completely hidden if the Opportunity integration is enabled

--- app/code/community/TNW/Salesforce/Block/Sales/Order/View/Tab/Salesforce/Form.php
+++ app/code/community/TNW/Salesforce/Block/Sales/Order/View/Tab/Salesforce/Form.php
@@ -24,50 +24,51 @@ class TNW_Salesforce_Block_Sales_Order_View_Tab_Salesforce_Form extends Mage_Adm
      */
     protected function _prepareForm()
     {
-        $form = new Varien_Data_Form(array(
-            'id' => 'edit_form',
-            'action' => $this->getUrl(
-                '*/sales_order/saveSalesforce',
-                array(
-                    'order_id' => $this->getOrder()->getId(),
-                )
-            ),
-            'method' => 'post',
-            'enctype' => 'multipart/form-data'
-        ));
-        $form->setUseContainer(true);
-
-        $fieldset = $form->addFieldset('fields', array('legend' => Mage::helper('tnw_salesforce')->__('Salesforce order data')));
-
-        /**
-         * Initialize product object as form property
-         * for using it in elements generation
-         */
-        $form->setFieldNameSuffix('order');
-
         if (Mage::helper('tnw_salesforce')->getOrderObject() != TNW_Salesforce_Model_Config_Objects::OPPORTUNITY_OBJECT) {
+            $form = new Varien_Data_Form(array(
+                'id' => 'edit_form',
+                'action' => $this->getUrl(
+                        '*/sales_order/saveSalesforce',
+                        array(
+                            'order_id' => $this->getOrder()->getId(),
+                        )
+                    ),
+                'method' => 'post',
+                'enctype' => 'multipart/form-data'
+            ));
+            $form->setUseContainer(true);
+
+            $fieldset = $form->addFieldset('fields', array('legend' => Mage::helper('tnw_salesforce')->__('Salesforce order data')));
+
+            /**
+             * Initialize product object as form property
+             * for using it in elements generation
+             */
+            $form->setFieldNameSuffix('order');
+
             $fieldset->addField('opportunity_id', 'text', array(
                 'label' => $this->__('Opportunity ID'),
                 'name' => 'opportunity_id',
             ));
+
+            $fieldset->addField('submit', 'note',
+                array(
+                    'text' => $this->getLayout()->createBlock('adminhtml/widget_button')
+                            ->setData(array(
+                                'label' => Mage::helper('tnw_salesforce')->__('Save'),
+                                'onclick' => 'this.form.submit();',
+                                'class' => 'save'
+                            ))
+                            ->toHtml(),
+                )
+            );
+
+
+            $form->setValues($this->getOrder()->getData());
+
+
+            $this->setForm($form);
         }
-
-        $fieldset->addField('submit', 'note',
-            array(
-                'text' => $this->getLayout()->createBlock('adminhtml/widget_button')
-                    ->setData(array(
-                        'label' => Mage::helper('tnw_salesforce')->__('Save'),
-                        'onclick' => 'this.form.submit();',
-                        'class' => 'save'
-                    ))
-                    ->toHtml(),
-            )
-        );
-
-
-        $form->setValues($this->getOrder()->getData());
-
-        $this->setForm($form);
     }
 
 }
