5f9aa14ba40d708bab5b51724da148dc4376f840
Evgeniy <eermolaev@yandex.ru>
2015-08-20 17:24:15 +0300

    PCMI-43 - Remove old getProductAttributesSync() method

--- app/code/community/TNW/Salesforce/Helper/Data.php
+++ app/code/community/TNW/Salesforce/Helper/Data.php
@@ -35,7 +35,6 @@ class TNW_Salesforce_Helper_Data extends TNW_Salesforce_Helper_Abstract
     /* Product */
     const PRODUCT_SYNC = 'salesforce_product/general/product_enable';
     const PRODUCT_PRICEBOOK = 'salesforce_product/general/default_pricebook';
-    const PRODUCT_ATTRIBUTES_SYNC = 'salesforce_product/general/product_attributes_sync';
 
     // newsletter related
     const CUSTOMER_NEWSLETTER_SYNC = 'salesforce_customer/newsletter_config/customer_newsletter_enable_sync';
@@ -406,11 +405,6 @@ class TNW_Salesforce_Helper_Data extends TNW_Salesforce_Helper_Abstract
     }
 
     // Salesforce default Pricebook used to store Product prices from Magento
-
-    public function getProductAttributesSync()
-    {
-        return $this->getStroreConfig(self::PRODUCT_ATTRIBUTES_SYNC);
-    }
 
     public function getCustomerNewsletterSync()
     {
