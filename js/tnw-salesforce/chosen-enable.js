f5596f56a61059f31f6e344215fda79922d7fce5
Evgeniy <eermolaev@yandex.ru>
2015-09-22 17:59:21 +0300

    PCMI-275 - Product Campaigns (Enterprise version only)

--- js/tnw-salesforce/chosen-enable.js
+++ js/tnw-salesforce/chosen-enable.js
@@ -1,5 +1,8 @@
 document.observe('dom:loaded', function () {
     $$('.chosen-select').each(function (element) {
+        /**
+         * Why ?..
+         */
         element.removeAttribute('disabled');
         new Chosen(element, {width: '280px', allow_single_deselect: true });
     });
