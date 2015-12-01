db46be5aedf18c02752d15792641420c2b88f3c4
Evgeniy Kalashnikov <kenbo@prola.ru>
2015-12-01 10:56:17 +0300

    PCMI-394 - Campaign newsletter bug It was added new source model

---
+++ app/code/community/TNW/Salesforce/sql/tnw_salesforce_setup/mysql4-upgrade-0.01.53.13.0000-0.01.53.14.0000.php
@@ -0,0 +1,10 @@
+<?php
+
+/** @var TNW_Salesforce_Model_Mysql4_Setup $installer */
+$installer = $this;
+$installer->startSetup();
+
+$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+$setup->updateAttribute('catalog_product', 'salesforce_campaign_id', 'source_model', 'tnw_salesforce/system_config_source_product_campaign');
+
+$installer->endSetup();
