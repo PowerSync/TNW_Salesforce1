1b098a7f38d011e812c4ef78b844bd198b0b158c
Evgeniy Kalashnikov <kenbo@prola.ru>
2015-12-11 16:42:00 +0300

    PCMI-387 - Advanced lookup Import and manage page FIX: Get account chunk.

--- app/code/community/TNW/Salesforce/data/tnw_salesforce_setup/data-upgrade-0.01.53.12.5000-0.01.53.13.0000.php
+++ app/code/community/TNW/Salesforce/data/tnw_salesforce_setup/data-upgrade-0.01.53.12.5000-0.01.53.13.0000.php
@@ -13,38 +13,40 @@ $tableName = $installer->getTable('tnw_salesforce/account_matching');
 
 $conf = Mage::getStoreConfig('salesforce_customer/account_catchall/domains');
 if (!empty($conf) && ($data = @unserialize($conf)) && is_array($data) && array_key_exists('account', $data) && is_array($data['account'])) {
-    /** @var TNW_Salesforce_Model_Api_Entity_Resource_Account_Collection $collection */
-    $collection = Mage::getModel('tnw_salesforce_api_entity/account')->getCollection();
-    try {
-        $toOptionHash = $collection->toOptionHashCustom();
-    } catch(Exception $e) {
-        $toOptionHash = array();
-    }
 
-    // Prepare Data
-    $prepareDate = array();
-    foreach($data['account'] as $key => $accountId) {
-        if (empty($accountId)) {
-            continue;
-        }
-
-        $accountName = '';
-        if (isset($toOptionHash[$accountId])) {
-            $accountName = $toOptionHash[$accountId];
-        }
-
-        $prepareDate[] = array(
-            'account_name' => $accountName,
-            'account_id'   => $accountId,
-            'email_domain' => $data['domain'][$key],
-        );
-    }
-
-    // Save Data
-    $chunkData = array_chunk($prepareDate, 200, true);
+    $chunkData = array_chunk($data['account'], TNW_Salesforce_Helper_Data::BASE_UPDATE_LIMIT, true);
     foreach ($chunkData as $chunkItem) {
+        /** @var TNW_Salesforce_Model_Api_Entity_Resource_Account_Collection $collection */
+        $collection = Mage::getModel('tnw_salesforce_api_entity/account')->getCollection();
+        $collection->addFieldToFilter('Id', array('in' => $chunkItem));
+
         try {
-            $installer->getConnection()->insertMultiple($tableName, $chunkItem);
+            $toOptionHash = $collection->toOptionHashCustom();
+        } catch(Exception $e) {
+            $toOptionHash = array();
+        }
+
+        // Prepare Data
+        $prepareDate = array();
+        foreach($chunkItem as $key => $accountId) {
+            if (empty($accountId)) {
+                continue;
+            }
+
+            $accountName = '';
+            if (isset($toOptionHash[$accountId])) {
+                $accountName = $toOptionHash[$accountId];
+            }
+
+            $prepareDate[] = array(
+                'account_name' => $accountName,
+                'account_id'   => $accountId,
+                'email_domain' => $data['domain'][$key],
+            );
+        }
+
+        try {
+            $installer->getConnection()->insertMultiple($tableName, $prepareDate);
         } catch(Exception $e) {
             Mage::logException($e);
         }
