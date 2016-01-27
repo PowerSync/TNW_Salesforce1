a2bff9f2ff30b347c89e61a850dcdee19f25bc51
Evgeniy Kalashnikov <kenbo@prola.ru>
2016-01-27 17:29:32 +0300

    PCMI-572 - Set max possible incremen_id for DB logger Increased the size of the field "entity_id" at the table logs

--- app/code/community/TNW/Salesforce/sql/tnw_salesforce_setup/mysql4-upgrade-0.01.53.14.5000-0.01.53.15.0000.php
+++ app/code/community/TNW/Salesforce/sql/tnw_salesforce_setup/mysql4-upgrade-0.01.53.14.5000-0.01.53.15.0000.php
@@ -10,7 +10,7 @@ $setup = new Mage_Eav_Model_Entity_Setup('core_setup');
 $installer->getConnection()->modifyColumn(
     $installer->getTable('tnw_salesforce/log'),
     'entity_id',
-    'BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT \'ID Field\','
+    'BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT \'ID Field\''
 );
 
 $installer->endSetup();
