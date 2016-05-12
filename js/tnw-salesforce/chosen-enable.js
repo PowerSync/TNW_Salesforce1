6ddb30ee5872447b0599db3a4d4bd3f2f871ee51
eermolaev <eermolaev@powersync.biz>
2016-05-12 16:45:12 +0400

    PCMI-807 - Chosen plugin improvement

--- js/tnw-salesforce/chosen-enable.js
+++ js/tnw-salesforce/chosen-enable.js
@@ -1,6 +1,6 @@
 document.observe('dom:loaded', function () {
     $$('.chosen-select').each(function (element) {
-        new Chosen(element, {width: '280px', allow_single_deselect: true });
+        new Chosen(element, {width: '280px', allow_single_deselect: true, disable_search_threshold: 5 });
     });
 });
 
@@ -11,3 +11,53 @@ FormElementDependenceController.prototype.trackChange = function(e, idTo, values
     this.oldTrackChange(e, idTo, valuesFrom);
     $(idTo).fire("chosen:updated");
 };
+
+Element.prototype.oldToggleValueElements = Element.prototype.toggleValueElements;
+
+Element.prototype.toggleValueElements = function (checkbox, container, excludedElements, checked){
+    if(container && checkbox){
+        var ignoredElements = [checkbox];
+        if (typeof excludedElements != 'undefined') {
+            if (Object.prototype.toString.call(excludedElements) != '[object Array]') {
+                excludedElements = [excludedElements];
+            }
+            for (var i = 0; i < excludedElements.length; i++) {
+                ignoredElements.push(excludedElements[i]);
+            }
+        }
+        //var elems = container.select('select', 'input');
+        var elems = Element.select(container, ['select', 'input', 'textarea', 'button', 'img']);
+        var isDisabled = (checked != undefined ? checked : checkbox.checked);
+        elems.each(function (elem) {
+            if (checkByProductPriceType(elem)) {
+                var i = ignoredElements.length;
+                while (i-- && elem != ignoredElements[i]);
+                if (i != -1) {
+                    return;
+                }
+                elem.disabled = isDisabled;
+                if (isDisabled) {
+                    elem.addClassName('disabled');
+                } else {
+                    elem.removeClassName('disabled');
+                }
+                /**
+                 * update chosen widget too
+                 * code below is necessary because 'fire' not works with disabled elements
+                 */
+                if (elem.hasClassName('chosen-select')) {
+                    elem.fire("chosen:updated");
+                    var registry = Element.retrieve(elem, 'prototype_event_registry');
+                    var eventObj = registry.get('chosen:updated');
+
+                    var eventHandlers = eventObj.pluck('handler');
+                    eventHandlers[0](null);
+                }
+
+                if (elem.nodeName.toLowerCase() == 'img') {
+                    isDisabled ? elem.hide() : elem.show();
+                }
+            }
+        });
+    }
+}
