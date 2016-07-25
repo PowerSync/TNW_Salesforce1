8dca607e2cefca5fcfeb6741941f657a86feef4e
Evgeniy Kalashnikov <kenbo@prola.ru>
2016-07-25 11:26:49 +0400

    PCMI-1033 - Synchronizing Activated Order FIX Notice

--- app/code/community/TNW/Salesforce/Helper/Salesforce/Abstract.php
+++ app/code/community/TNW/Salesforce/Helper/Salesforce/Abstract.php
@@ -1030,7 +1030,7 @@ class TNW_Salesforce_Helper_Salesforce_Abstract
      */
     public function logNotice($message)
     {
-        return Mage::getSingleton('tnw_salesforce/tool_log')->saveTrace($message);
+        return Mage::getSingleton('tnw_salesforce/tool_log')->saveNotice($message);
     }
 
     /**
