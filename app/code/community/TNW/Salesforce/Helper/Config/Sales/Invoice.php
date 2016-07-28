ae6e2cd8def5b1c19f9289b779e3224d1068929c
Evgeniy Kalashnikov <kenbo@prola.ru>
2016-07-28 15:48:27 +0400

    PCMI-1001 - Magento Configuration: Sales Configuration

--- app/code/community/TNW/Salesforce/Helper/Config/Sales/Invoice.php
+++ app/code/community/TNW/Salesforce/Helper/Config/Sales/Invoice.php
@@ -6,12 +6,21 @@
 
 class TNW_Salesforce_Helper_Config_Sales_Invoice extends TNW_Salesforce_Helper_Config_Sales
 {
-    const INVOICE_SYNC_ENABLE = 'salesforce_order/invoice_configuration/invoice_sync_enable';
+    const INVOICE_SYNC_ENABLE = 'salesforce_invoice/invoice_configuration/sync_enable';
+    const INVOICE_NOTES_SYNC  = 'salesforce_invoice/invoice_configuration/notes_synchronize';
 
     // Allow Magento to synchronize invoices with Salesforce
     public function syncInvoices()
     {
         return (int)$this->getStoreConfig(self::INVOICE_SYNC_ENABLE);
+    }
+
+    /**
+     * @return int
+     */
+    public function syncInvoiceNotes()
+    {
+        return (int)$this->getStoreConfig(self::INVOICE_NOTES_SYNC);
     }
 
     /**
