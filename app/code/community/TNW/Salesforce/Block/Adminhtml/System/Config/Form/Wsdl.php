0d29648d5a6145e62f49a6a8b37a9bf432ea31e9
Evgeniy Kalashnikov <kenbo@prola.ru>
2016-07-29 10:44:14 +0400

    PCMI-1034 - WSDL uploading from

--- app/code/community/TNW/Salesforce/Block/Adminhtml/System/Config/Form/Wsdl.php
+++ app/code/community/TNW/Salesforce/Block/Adminhtml/System/Config/Form/Wsdl.php
@@ -10,12 +10,22 @@ class TNW_Salesforce_Block_Adminhtml_System_Config_Form_Wsdl extends Mage_Adminh
      */
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element)
     {
-        $element->setData('after_element_html', '<div id="import_form" style="display: inline;"><span class="fileUpload">
-    <input type="button" class="form-button" value="Browse..." />
-    <input id="uploadBtn" type="file" class="upload" name="fileImport" />
-</span><button title="Save" type="button" class="scalable" onclick="configForm.submit()" style=""><span><span><span>Save</span></span></span></button></div>');
 
-        $element->setData('style', 'width: 149px;');
-        return $element->getElementHtml();
+        $html = <<<HTML
+<span class="fileUpload">
+    <input type="button" class="form-button" value="{$this->__('Browse...')}" />
+    <input id="uploadBtn" type="file" class="upload" name="{$element->getData('name')}" style="width:80px;" />
+</span>
+<button title="{$this->__('Save')}" type="button" class="scalable" onclick="configForm.submit()" style="">
+    <span><span><span>{$this->__('Save')}</span></span></span>
+</button>
+HTML;
+
+        $element->addData(array(
+            'after_element_html' => $html,
+            'style'              => 'width: 149px;'
+        ));
+
+        return sprintf('<div id="import_form">%s</div>', $element->getElementHtml());
     }
 }
--- app/code/community/TNW/Salesforce/Model/System/Config/Backend/Wsdl.php
+++ app/code/community/TNW/Salesforce/Model/System/Config/Backend/Wsdl.php
@@ -2,5 +2,57 @@
 
 class TNW_Salesforce_Model_System_Config_Backend_Wsdl extends Mage_Adminhtml_Model_System_Config_Backend_File
 {
+    /**
+     * Save uploaded file before saving config value
+     *
+     * @return $this
+     */
+    protected function _beforeSave()
+    {
+        if ($_FILES['groups']['tmp_name'][$this->getGroupId()]['fields'][$this->getField()]['value']){
 
+            $uploadDir = $this->_getUploadDir();
+
+            try {
+                $file = array();
+                $tmpName = $_FILES['groups']['tmp_name'];
+                $file['tmp_name'] = $tmpName[$this->getGroupId()]['fields'][$this->getField()]['value'];
+                $name = $_FILES['groups']['name'];
+                $file['name'] = $name[$this->getGroupId()]['fields'][$this->getField()]['value'];
+                $uploader = new Mage_Core_Model_File_Uploader($file);
+                $uploader->setAllowedExtensions($this->_getAllowedExtensions());
+                $uploader->setAllowRenameFiles(false);
+                $uploader->addValidateCallback('size', $this, 'validateMaxSize');
+                $result = $uploader->save($uploadDir);
+
+            } catch (Exception $e) {
+                Mage::throwException($e->getMessage());
+            }
+
+            $filename = $result['file'];
+            if ($filename) {
+                if ($this->_addWhetherScopeInfo()) {
+                    $filename = $this->_prependScopeInfo($filename);
+                }
+
+                $fieldConfig = $this->getFieldConfig();
+                /* @var $fieldConfig Varien_Simplexml_Element */
+                $uploadDir = (string)$fieldConfig->upload_dir;
+                $this->setValue('var/'.$uploadDir.'/'.$filename);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Return the root part of directory path for uploading
+     *
+     * @var string
+     * @return string
+     */
+    protected function _getUploadRoot($token)
+    {
+        return Mage::getBaseDir('var');
+    }
 }
