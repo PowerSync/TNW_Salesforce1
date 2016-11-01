0a532995fdd7661f40558a77bf17afc7798098a1
Evgeniy Kalashnikov <kenbo73@gmail.com>
2016-11-01 14:48:17 +0400

    PCMI-1409 - Questions (#358)
    * PCMI-1409 - Questions
    
    * PCMI-1409 - Questions
    FIX
    
    * PCMI-1409 - Questions
    FIX

---
+++ app/code/community/TNW/Salesforce/Model/Sale/Order/Create/Session/Quote.php
@@ -0,0 +1,10 @@
+<?php
+
+class TNW_Salesforce_Model_Sale_Order_Create_Session_Quote extends Mage_Adminhtml_Model_Session_Quote
+{
+    public function __construct()
+    {
+        // Disable Init session
+        return;
+    }
+}
