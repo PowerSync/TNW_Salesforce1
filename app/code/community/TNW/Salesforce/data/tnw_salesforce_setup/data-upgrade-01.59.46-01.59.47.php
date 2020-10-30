5d18e6ff1c2244d20a4a03d1733610dd30244111
eermolaev <eermolaev@yandex.ru>
2020-10-30 22:23:58 +0400

    181
    Signed-off-by: eermolaev <eermolaev@yandex.ru>

--- app/code/community/TNW/Salesforce/data/tnw_salesforce_setup/data-upgrade-01.59.46-01.59.47.php
+++ app/code/community/TNW/Salesforce/data/tnw_salesforce_setup/data-upgrade-01.59.46-01.59.47.php
@@ -3,44 +3,55 @@
  * @var $this TNW_Salesforce_Model_Mysql4_Setup
  */
 
+/**
+ * @var $this TNW_Salesforce_Model_Mysql4_Setup
+ */
+$installer = $this;
+
+$setup = new Mage_Catalog_Model_Resource_Setup('core_setup');
+
 $entityTypeCode = 'catalog_product';
 
 $groupName = 'General';
 
 $groups = Mage::getModel('eav/entity_attribute_group')->getCollection();
 $groups->getSelect()
-    ->joinLeft(
+    ->joinInner(
         'eav_attribute_set',
         'main_table.attribute_set_id = eav_attribute_set.entity_type_id',
         array('eav_attribute_set.entity_type_id')
-    )->joinLeft(
+    )->joinInner(
         'eav_entity_type',
         'eav_attribute_set.entity_type_id = eav_entity_type.entity_type_id',
         array('entity_type_code')
     )->where("main_table.attribute_group_name = 'General' AND eav_entity_type.entity_type_code = 'catalog_product'");
 $currentGroup = $groups->getFirstItem();
 
-$subquery = new \Zend_Db_Expr('SELECT `attribute_group_id` FROM `eav_attribute_group`');
-
 $attributes = Mage::getModel('eav/entity_attribute')->getCollection();
-$attributes->getSelect()->distinct(true)
-    ->joinLeft(
-    'eav_entity_attribute',
-    'main_table.attribute_id = eav_entity_attribute.attribute_id',
+$attributes->getSelect()
+    ->joinInner(
+        'eav_entity_attribute',
+        'main_table.attribute_id = eav_entity_attribute.attribute_id',
         array(
             'eav_entity_attribute.attribute_group_id',
             'eav_entity_attribute.attribute_set_id',
             'eav_entity_attribute.entity_attribute_id',
         )
-    )->joinLeft(
+    )->joinInner(
         'eav_entity_type',
         'eav_entity_attribute.entity_type_id = eav_entity_type.entity_type_id',
         array('eav_entity_type.entity_type_code')
-    )->where("eav_entity_attribute.attribute_group_id NOT IN ($subquery)");
+    )->joinLeft(
+        'eav_attribute_group',
+        'eav_entity_attribute.attribute_group_id = eav_attribute_group.attribute_group_id'
+    )
+    ->where("eav_attribute_group.attribute_group_id IS NULL ");
 
-$items = $attributes->addFieldToFilter('entity_type_code', ['eq' => $entityTypeCode])->getItems();
+$itemIds = $attributes->addFieldToFilter('entity_type_code', ['eq' => $entityTypeCode])->getAllIds();
 
-foreach ($items as $item) {
-    $item->setData('attribute_group_id', $currentGroup->getAttributeGroupId());
-    $item->save();
-}
+$installer->getConnection()->update('eav_entity_attribute', array(
+    'attribute_group_id' => $currentGroup->getId()
+), array('attribute_id' => $itemIds));
+
+
+$installer->endSetup();
